e2eac20be1b35e9cfdddfd3ab1cdfe8aac57e904
Linusp <linusp1024@gmail.com>
2017-07-16 10:25:36 +0800

    disable obsolete service types: Recognize, SingleCharRecognize

--- baiduocr/core.py
+++ baiduocr/core.py
@@ -3,28 +3,19 @@
 from __future__ import absolute_import, print_function
 import requests
 
-from baiduocr.result import (
-    LocateRecognizeResult,
-    SingleCharResult,
-)
+from baiduocr.result import LocateRecognizeResult
 
 
 _API_URL = 'http://apis.baidu.com/idl_baidu/baiduocrpay/idlocrpaid'
-_SERVICE_RESULT_MAP = {
-    'Recognize': LocateRecognizeResult,
-    'LocateRecognize': LocateRecognizeResult,
-    'Locate': LocateRecognizeResult,
-    'SingleCharRecognize': SingleCharResult,
-}
 
 
 class BaiduOcr(object):
     """百度 OCR 客户端"""
 
-    _IMAGE_FOR_TEST = '/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDABMNDxEPDBMREBEWFRMXHTAfHRsbHTsqLSMwRj5KSUU+RENNV29eTVJpU0NEYYRiaXN3fX59S12Jkoh5kW96fXj/2wBDARUWFh0ZHTkfHzl4UERQeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHj/wAARCAAfACEDAREAAhEBAxEB/8QAGAABAQEBAQAAAAAAAAAAAAAAAAQDBQb/xAAjEAACAgICAgEFAAAAAAAAAAABAgADBBESIRMxBSIyQXGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/APawEBAQEBAgy8i8ZTVV3UY6V1eU2XoWDDZB19S646Gz39w9fkKsW1r8Wm2yo1PYis1be0JG9H9QNYCAgc35Cl3yuVuJZl0cB41rZQa32dt2y6OuOiOxo61vsLcVblxaVyXD3hFFjL6La7I/sDWAgICAgICB/9k='
+    _IMAGE_FOR_TEST = '/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDABMNDxEPDBMREBEWFRMXHTAfHRsbHTsqLSMwRj5KSUU+RENNV29eTVJpU0NEYYRiaXN3fX59S12Jkoh5kW96fXj/2wBDARUWFh0ZHTkfHzl4UERQeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHj/wAARCAAfACEDAREAAhEBAxEB/8QAGAABAQEBAQAAAAAAAAAAAAAAAAQDBQb/xAAjEAACAgICAgEFAAAAAAAAAAABAgADBBESIRMxBSIyQXGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/APawEBAQEBAgy8i8ZTVV3UY6V1eU2XoWDDZB19S646Gz39w9fkKsW1r8Wm2yo1PYis1be0JG9H9QNYCAgc35Cl3yuVuJZl0cB41rZQa32dt2y6OuOiOxo61vsLcVblxaVyXD3hFFjL6La7I/sDWAgICAgICB/9k='  # pylint: disable=line-too-long
     _IMAGE_MAX_SIZE = 307200    # 300K
 
-    _SERVICE_LIST = set(['LocateRecognize', 'Locate', 'Recognize', 'SingleCharRecognize'])
+    _SERVICE_LIST = set(['LocateRecognize', 'Locate'])
     _LANG_LIST = set(['CHN_ENG', 'ENG', 'JAP', 'KOR'])
 
     def __init__(self, url=_API_URL, key=''):
@@ -50,21 +41,21 @@ class BaiduOcr(object):
         data = {}
         data['fromdevice'] = 'pc'
         data['clientip'] = '10.10.10.0'
-        data['detecttype'] = 'Recognize'
+        data['detecttype'] = 'LocateRecognize'
         data['languagetype'] = 'CHN_ENG'
         data['imagetype'] = '1'
         data['image'] = self._IMAGE_FOR_TEST
 
         resp = requests.post(self.url, headers=header, data=data)
         res = resp.json() if resp is not None else {}
-        res = _SERVICE_RESULT_MAP['Recognize'](res)
+        res = LocateRecognizeResult(res)
 
         if res.status == 0 and res.message == u'success':
             print('pong')
 
         return res
 
-    def recog(self, image, service='Recognize', lang='CHN_ENG'):
+    def recog(self, image, service='LocateRecognize', lang='CHN_ENG'):
         """调用百度 OCR API 进行图片文字识别
 
         :type image: str
@@ -75,8 +66,6 @@ class BaiduOcr(object):
         :param service: 请求的服务类型，可用的服务类型即说明如下
                         + LocateRecognize: 整图文字检测、识别，以行为单位
                         + Locate: 整图文字行定位
-                        + Recognize: 整图文字识别(默认)
-                        + SingleCharRecognize: 单字图像识别
 
         :type lang: str
         :param lang: 指定要检测的文字类型，目前仅支持以下类型
@@ -110,7 +99,8 @@ class BaiduOcr(object):
         except Exception:
             return {}
 
-        resp = requests.post(self.url, headers=header, data=data, files={'image': ('ocr.jpg', image_file)})
+        resp = requests.post(self.url, headers=header, data=data,
+                             files={'image': ('ocr.jpg', image_file)})
         resp = {} if not resp else resp.json()
 
-        return _SERVICE_RESULT_MAP[service](resp)
+        return LocateRecognizeResult(resp)
--- baiduocr/result.py
+++ baiduocr/result.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-from operator import itemgetter
 from baiduocr.consts import (
     ERROR_NO_RESULT,
 )
@@ -9,6 +8,7 @@ from baiduocr.consts import (
 
 class Rect(object):
     """矩形区域"""
+
     def __init__(self, top=0, left=0, width=0, height=0):
         self.top = int(top)
         self.left = int(left)
@@ -50,46 +50,14 @@ class LocateRecognizeResult(Result):
         if self.result:
             res = u'\n'.join([text for text, _ in self.result]).strip()
 
-        if not res or len(res) == 0:
+        if not res:
             return u'Error: No text result'
-        else:
-            return res
+
+        return res
 
     def get_result_regions(self):
         """获取识别结果中的区域信息"""
         if self.result:
             return [region for _, region in self.result]
-        else:
-            return []
 
-
-class SingleCharResult(Result):
-    """service type 为 SingleCharRecognize 时的结果"""
-
-    def __init__(self, response):
-        """初始化，将 result 转换成更易处理的格式"""
-        super(SingleCharResult, self).__init__(response)
-        self.result = [
-            (row.get(u'word'), float(row.get(u'prob')))
-            for row in self.result
-        ] if self.result else []
-        self.result = sorted(self.result, key=itemgetter(1), reverse=True)
-        self.threshold = 0.50
-
-    def get_threshold(self):
-        """获取当前的拒绝阈值，识别结果中置信度低于该阈值的结果将会被忽视"""
-        return self.threshold
-
-    def set_threshold(self, threshold):
-        """设定拒绝阈值"""
-        self.threshold = threshold
-
-    def get_char(self):
-        """获取识别结果中置信度最大的字符，这里会通过拒绝阈值来进行过滤"""
-        char = None
-        if self.result:
-            char, prob = self.result[0]
-            if prob < self.threshold:
-                char = None
-
-        return char if char else 'Error: No character result'
+        return []
